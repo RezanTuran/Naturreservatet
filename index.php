4f4a2fbfe9aa95361a7d4acea8b6309a829442fa
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2020-01-14 10:47:56 +0100

    Done form and input

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Naturreservatet</title>
+</head>
+<body>
+    
+    <form action="results.php" method="post">
+    
+       Apor: <input type="number" name="" id="">
+       <br>
+       Giraffer: <input type="number" name="" id="">
+       <br>
+       Tigrar: <input type="number" name="" id="">
+       <br>
+       Kokosnötter: <input type="number" name="" id=""> 
+       <br>
+            <input type="submit" value="Result">
+
+    </form>
+
+
+</body>
+</html>
