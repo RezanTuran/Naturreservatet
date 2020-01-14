ef65b01a4e881724df4a3a2353cf50474dbafaa0
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2020-01-14 15:43:47 +0100

    Done all backend job

--- index.php
+++ index.php
@@ -4,24 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Naturreservatet</title>
 </head>
 <body>
     
     <form action="results.php" method="post">
     
-       Apor: <input type="number" name="" id="">
+       Apor: <input type="number" name="apa" id="apa">
+    
+       Giraffer: <input type="number" name="giraff" id="giraff">
+       
+       Tigrar: <input type="number" name="tiger" id="tiger">
+       
+       Kokosnötter: <input type="number" name="kokosnötter" id="kokosnötter"> 
        <br>
-       Giraffer: <input type="number" name="" id="">
        <br>
-       Tigrar: <input type="number" name="" id="">
-       <br>
-       Kokosnötter: <input type="number" name="" id=""> 
-       <br>
-            <input type="submit" value="Result">
+        <input type="submit" value="Result">
 
     </form>
 
 
 </body>
 </html>
+
--- results.php
+++ results.php
@@ -1,14 +1,98 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-    <h1>Welcome</h1>
 
-</body>
-</html>
+<?php
+
+abstract class Animal {
+    public $name;
+    public $imageUrl;
+    
+    abstract function makeSound();
+}
+
+class Apa extends Animal {
+    function __construct($name) {
+        $this->name = $name;
+        $this->imageUrl = "./img/apa.jpg";
+    }
+    public function makeSound() {
+        return "Whoohoho";
+    }
+}
+
+class Giraff extends Animal {
+    function __construct($name) {
+        $this->name = $name;
+        $this->imageUrl = "./img/giraff.jpg";
+    }
+    public function makeSound() {
+        return "Zrrrr";
+    }
+}
+
+class Tiger extends Animal {
+    function __construct($name) {
+        $this->name = $name;
+        $this->imageUrl = "./img/tiger.jpg";
+    }
+    public function makeSound() {
+        return "Xrrrr";
+    }
+}
+
+class Kokosnötter extends Animal {
+    function __construct($name) {
+        $this->name = $name;
+        $this->imageUrl = "./img/kokosnötter.jpg";
+    }
+    public function makeSound() {
+        return "Pitpitpti";
+    }
+}
+
+
+$nrOfMonkeys = $_POST['apa'];
+$nrOfGiraff = $_POST['giraff'];
+$nrOfTiger = $_POST['tiger'];
+$nrOfKokosnötter =$_POST['kokosnötter'];
+
+$animalArray = array();
+
+for ($i=0; $i < $nrOfMonkeys; $i++) { 
+    $apa = new Apa("Johan");
+    array_push($animalArray, $apa);
+};
+for ($i=0; $i < $nrOfGiraff; $i++) { 
+    $giraff = new Giraff("Kalle");
+    array_push($animalArray, $giraff);
+};
+for ($i=0; $i < $nrOfTiger; $i++) { 
+    $tiger = new Tiger("Glenn");
+    array_push($animalArray, $tiger);
+};
+for ($i=0; $i < $nrOfKokosnötter; $i++) { 
+    $kokosnötter = new Kokosnötter("Martin");
+    array_push($animalArray, $kokosnötter);
+};
+
+
+foreach($animalArray as $value){
+    echo ($value->name);
+    echo "<br>";
+    echo "<img style='width: 10em; height: auto;' src='". $value->imageUrl ."'>" . "<br>". $value->makeSound(). "<br>";
+}
+
+
+
+
+// echo $apa ->makeSound();
+// echo "<br>";
+// echo $giraff ->makeSound();
+// echo "<br>";
+// echo $tiger ->makeSound();
+// echo "<br>";
+// echo $kokosnötter ->makeSound();
+?>
+
+    
+
+
+
